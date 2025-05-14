4082c445cd1e5c40eb930fe6aef5489ad0ad7457
IhantsaFana <ihantsarakotondranaivo@gmail.com>
2025-05-14 12:02:07 +0300

    OK

---
+++ tests/test_views.py
@@ -0,0 +1,13 @@
+from movies.views.default import my_view
+from movies.views.notfound import notfound_view
+
+
+def test_my_view(app_request):
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['project'] == 'movies'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
